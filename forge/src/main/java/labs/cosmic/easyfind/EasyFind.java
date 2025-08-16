ecf9b000c67ea1742694a60b6aa769754eb1b603
Sora <34794115+sorae42@users.noreply.github.com>
2025-08-16 18:15:34 +0700

    chore: move to multiloader (under renaming, not ready to use)

---
+++ forge/src/main/java/labs/cosmic/easyfind/EasyFind.java
@@ -0,0 +1,10 @@
+package labs.cosmic.easyfind;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class EasyFind {
+    public EasyFind() {
+        EasyFindCommon.init();
+    }
+}
