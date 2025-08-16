6b481dc6720128c106ab07a386214726159db8b6
manywonders <manywonders.gitme@proton.me>
2025-08-16 18:15:34 +0700

    chore: move to multiloader (under renaming, not ready to use)

---
+++ common/src/main/java/labs/cosmic/easyfind/screens/Spotlight.java
@@ -0,0 +1,233 @@
+package labs.cosmic.easyfind.screens;
+
+import labs.cosmic.easyfind.config.ConfigAgent;
+import labs.cosmic.easyfind.handler.InventoryHandler;
+import labs.cosmic.easyfind.handler.SearchHandler;
+import labs.cosmic.easyfind.screens.widgets.ResultListWidget;
+import labs.cosmic.easyfind.screens.widgets.ResultWidget;
+import labs.cosmic.easyfind.screens.widgets.SearchboxWidget;
+import labs.cosmic.easyfind.utils.ItemHistory;
+import labs.cosmic.easyfind.utils.SearchResult;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.Objects;
+import java.util.Queue;
+import java.util.function.BiConsumer;
+
+public class Spotlight extends Screen {
+    private SearchboxWidget searchboxWidget;
+    private ResultListWidget resultListWidget;
+
+    private final ItemHistory itemHistory;
+    private final SearchHandler searchManager;
+    private final InventoryHandler inventoryHandler;
+
+    private String prevQuery;
+    private Item lastClickItemEntry;
+
+    private static int slot;
+    private boolean isShiftDown = false;
+
+    private long lastClickTime;
+
+    final int inputHeight = 16;
+
+    LocalPlayer player;
+
+    public Spotlight(ItemHistory itemHistory) {
+        super(Component.nullToEmpty(I18n.get("efs.title")));
+        this.itemHistory = itemHistory;
+        this.searchManager = new SearchHandler();
+        this.inventoryHandler = new InventoryHandler(itemHistory);
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft != null) {
+            player = minecraft.player;
+        }
+
+        final int resultBoxWidth = Math.min(super.width / 2, 300);
+        final int resultBoxHeight = Math.min(super.height / 2, 300);
+        final int searchFieldX = super.width / 2 - resultBoxWidth / 2;
+        final int searchFieldY = (super.height / 6) + 6;
+        final int resultBoxX = super.width / 2 - resultBoxWidth / 2;
+        final int resultBoxY = (super.height / 6) + inputHeight + 6;
+
+        this.searchboxWidget = new SearchboxWidget(
+                this,
+                minecraft.font,
+                searchFieldX,
+                searchFieldY,
+                resultBoxWidth,
+                inputHeight
+        );
+
+        // Remove setChangedListener, setFocused, addDrawableChild
+        // Use addRenderableWidget for 1.20.1
+        super.addRenderableWidget(this.searchboxWidget);
+
+        resultListWidget = new ResultListWidget(
+                this,
+                minecraft,
+                resultBoxWidth,
+                resultBoxHeight,
+                resultBoxY + 1,
+                resultBoxY + resultBoxHeight
+        );
+        super.addRenderableWidget(resultListWidget);
+        this.updateResults();
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        if (ConfigAgent.darkenBG) this.renderBackground(context);
+        super.render(context, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public void tick() {
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft != null && minecraft.player != null) {
+            if (!minecraft.player.getAbilities().instabuild) this.close();
+        }
+    }
+
+    private void search(final String query) {
+        if (this.prevQuery != null && this.prevQuery.equals(query)) {
+            return;
+        }
+        this.prevQuery = query;
+        this.resultListWidget.children().clear();
+
+        if (query.isEmpty()) {
+            if (!ConfigAgent.saveHistory) return;
+            this.itemHistory.getItemHistory().forEach(this::addToResult);
+        } else {
+            searchManager.search(query).forEach(this::addToResult);
+        }
+
+        if (!resultListWidget.children().isEmpty()) {
+            resultListWidget.setSelected(resultListWidget.children().get(0));
+        } else {
+            resultListWidget.setSelected(null);
+        }
+        resultListWidget.setScrollAmount(0);
+    }
+
+    private void addToResult(Item item) {
+        boolean hasFeature = this.player.connection != null; // Feature check removed for now
+        if (!hasFeature && !ConfigAgent.showDisabledItem) return;
+        resultListWidget.children().add(new ResultWidget(Minecraft.getInstance().font, item, hasFeature));
+    }
+
+    private void check(final BiConsumer<Minecraft, ResultWidget> entryConsumer) {
+        final ResultWidget entry = this.resultListWidget.getSelectedOrNull();
+        if (entry == null) {
+            return;
+        }
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft == null || minecraft.player == null) {
+            return;
+        }
+        entryConsumer.accept(minecraft, entry);
+    }
+
+    private void selectEntry(final ResultWidget entry) {
+        this.resultListWidget.setSelected(entry);
+        if (!entry.getItem().equals(this.lastClickItemEntry)) {
+            this.lastClickItemEntry = null;
+        }
+    }
+
+    private boolean entryClickHandler(final double mouseY, final int button) {
+        // entry select
+        final int entryY = this.resultListWidget.getEntryY(mouseY);
+        if (entryY >= 0) {
+            final int entryIndex = entryY / this.resultListWidget.getEntryHeight();
+            final ResultWidget entry = this.resultListWidget.at(entryIndex);
+            if (entry != null) {
+                this.selectEntry(entry);
+            }
+        }
+
+        // double click exec
+        final ResultWidget selectedEntry = this.resultListWidget.getSelectedOrNull();
+        if (selectedEntry != null) {
+            final long timeMs = Util.getMeasuringTimeMs();
+            if (timeMs - this.lastClickTime <= 420
+                    && this.lastClickItemEntry != null
+                    && this.lastClickItemEntry.equals(selectedEntry.getItem())) {
+                if (button == 0) {
+                    this.giveItem();
+                }
+                return true;
+            }
+
+            this.lastClickTime = timeMs;
+            this.lastClickItemEntry = selectedEntry.getItem();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        this.isShiftDown = (modifiers & GLFW.GLFW_MOD_SHIFT) == GLFW.GLFW_MOD_SHIFT;
+        return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (this.resultListWidget.isMouseOver(mouseX, mouseY)
+                && this.entryClickHandler(mouseY, button)) {
+            return true;
+        }
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    // TODO: Configurable, also refactor this.
+    public void giveItem() {
+        this.check((client, entry) -> {
+            inventoryHandler.giveItem(client, entry.getItem());
+            this.close();
+        });
+    }
+
+    public void updateResults() {
+        this.search(this.searchboxWidget.getText());
+    }
+
+    public ResultListWidget getResultList() {
+        return resultListWidget;
+    }
+
+    public SearchboxWidget getSearchboxWidget() {
+        return searchboxWidget;
+    }
+
+    public Queue<Item> getItemHistory() {
+        return itemHistory.getItemHistory();
+    }
+
+    @Override
+    public void close() {
+        if (ConfigAgent.keepScreenOn == ConfigAgent.KeepScreen.SHIFT) {
+            if (this.isShiftDown) return;
+        }
+        super.close();
+    }
+}
---
+++ common/src/main/java/labs/cosmic/easyfind/screens/widgets/SearchboxWidget.java
@@ -0,0 +1,63 @@
+package labs.cosmic.easyfind.screens.widgets;
+
+import labs.cosmic.easyfind.screens.Spotlight;
+import labs.cosmic.easyfind.utils.SearchResult;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.network.chat.Component;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.function.BiConsumer;
+
+public class SearchboxWidget extends EditBox {
+    private final Spotlight spotlight;
+    private final Font font;
+    private final Component placeholder;
+    private BiConsumer<SearchResult, ResultWidget> resultConsumer = null;
+    
+    public SearchboxWidget(Spotlight screen, Font font, int x, int y, int width, int height) {
+        super(font, x, y, width, height, Component.translatable("efs.title"));
+        this.spotlight = screen;
+        this.font = font;
+        this.placeholder = Component.translatable("efs.placeholder").withStyle(ChatFormatting.GRAY);
+        this.setMaxLength(256);
+        this.setEditable(true);
+        // Placeholder logic will be handled in render
+    }
+    
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        // Move selection down on key down
+        if (keyCode == GLFW.GLFW_KEY_DOWN) {
+            this.spotlight.getResultList().selectNextEntryInDirection(NavigationDirection.DOWN);
+        }
+        final SearchResult result = SearchResult.fromKeyCode(keyCode);
+        if (result != null) {
+            final ResultWidget resultWidgetEntry = this.spotlight.getResultList().getSelected();
+            if (resultWidgetEntry != null) {
+                this.resultConsumer.accept(result, resultWidgetEntry);
+            }
+            return true;
+        }
+        return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+
+    @Override
+    public void render(net.minecraft.client.gui.GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+        super.render(graphics, mouseX, mouseY, delta);
+        if (this.placeholder != null && this.getValue().isEmpty()) {
+            graphics.drawString(
+                this.font,
+                this.placeholder,
+                this.getX() + 4,
+                this.getY() + (this.getHeight() - 8) / 2,
+                0xE0E0E0
+            );
+        }
+    }
+
+    public void setResultConsumer(BiConsumer<SearchResult, ResultWidget> resultConsumer) {
+        this.resultConsumer = resultConsumer;
+    }
+}
